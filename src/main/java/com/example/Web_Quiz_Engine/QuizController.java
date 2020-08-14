09bab28d7294e9fbe5124ad1450287c59bfc4d27
Daeryss <fortmayn@mail.ru>
2020-08-14 13:09:23 +0300

    db

--- src/main/java/com/example/Web_Quiz_Engine/QuizController.java
+++ src/main/java/com/example/Web_Quiz_Engine/QuizController.java
@@ -1,113 +1,94 @@
-package com.example.Web_Quiz_Engine;
+package engine;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.*;
 
 @RestController
 public class QuizController {
-    private List<Quiz> quizzes = new ArrayList<>();
-    private List<Answer> answers = new ArrayList<>();
-    private List<GetQuiz> getQuizs = new ArrayList<>();
-    private int num = 1;
 
-    public QuizController() {
-        answers.add(new Answer(false,"Wrong answer! Try again."));
-        answers.add(new Answer(true,"Congratulations, you are right!"));
-    }
+    @Autowired
+    QuizRepository quizRepository;
 
-
-    @GetMapping(path = "/api/quizzes/{id}")
-    public GetQuiz getQuizzes(@PathVariable int id, HttpServletResponse response){
-        try {
-            return getQuizs.get(id - 1);
-        } catch (Exception exc) {
-            throw new NotFoundRequest();
+    @GetMapping(path = "/api/quizzes/{id}", produces = "application/json")
+    public Object getApiQuiz(@PathVariable Integer id){
+        if(quizRepository.existsById(id)){
+            Quiz quiz = quizRepository.findById(id)
+                    .orElseThrow();
+            Map<String, Object> outmap = new TreeMap<>();
+            outmap.put("id", id);
+            outmap.put("title", quiz.getTitle());
+            outmap.put("text", quiz.getText());
+            outmap.put("options", quiz.getOptions());
+            return outmap;
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
     }
 
-    @GetMapping(path = "/id")
-    public List<Quiz> quizzes(){
-        return quizzes;
+    @PostMapping(path = "/api/quizzes", consumes = "application/json")
+    public Map<String,Object> addQuizzies (@Valid @RequestBody Quiz quiz) {
+        quiz = quizRepository.save(quiz);
+
+        TreeMap<String,Object> outmap = new TreeMap<>();
+        outmap.put("id", quiz.getId());
+        outmap.put("title", quiz.getTitle());;
+        outmap.put("text", quiz.getText());
+        outmap.put("options", quiz.getOptions());
+        return outmap;
     }
 
-    @GetMapping(path = "/api/quizzes")
-    public List<GetQuiz> getAllQuizzes(){
-        return getQuizs;
-    }
+    @GetMapping(path = "/api/quizzes", produces = "application/json")
+    public Object getAllQuiz(){
 
-    @PostMapping(path = "api/quizzes")
-    public GetQuiz addQuiz(@RequestBody @Valid  Quiz quiz) {
-        quizzes.add(quiz);
-        GetQuiz temp = new GetQuiz(num, quiz.getTitle(), quiz.getText(), quiz.getOptions());
-        getQuizs.add(temp);
-        num++;
-        return temp;
-    }
-    /*
-    @PostMapping(path = "api/quizzes")
-    public GetQuiz setQuizzes(@RequestBody Quiz quiz) {
-        if (quiz.getText().equals("") || quiz.getTitle().equals("") || quiz.getOptions().length < 2){
-            throw new BadRequestResponse();
-        } else {
-            quizzes.add(quiz);
-            GetQuiz temp = new GetQuiz(num, quiz.getTitle(), quiz.getText(), quiz.getOptions());
-            getQuizs.add(temp);
-            num++;
-            return temp;
-        }
-    }
-
-     */
-
-    @PostMapping(path = "/api/quizzes/{id}/solve")
-    public Answer getQuiz(@PathVariable int id, @RequestBody @Valid Solving solve){
-        String str = null;
-        for(int i = quizzes.get(id - 1).getAnswer().length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-                if( quizzes.get(id - 1).getAnswer()[j] > quizzes.get(id - 1).getAnswer()[j+1] ){
-                    int tmp = quizzes.get(id - 1).getAnswer()[j];
-                    quizzes.get(id - 1).getAnswer()[j] = quizzes.get(id - 1).getAnswer()[j+1];
-                    quizzes.get(id - 1).getAnswer()[j+1] = tmp;
-                }
+        ArrayList<Quiz> quizzes = (ArrayList<Quiz>) quizRepository.findAll();
+        if(quizzes.size()!=0){
+            ArrayList<HashMap<String, Object>> maps = new ArrayList<>();
+            for (Quiz q : quizzes
+            ) {
+                HashMap<String, Object> outmap = new HashMap<>();
+                outmap.put("id", q.getId());
+                outmap.put("title", q.getTitle());
+                outmap.put("text", q.getText());
+                outmap.put("options", q.getOptions());
+                maps.add(outmap);
             }
+            return maps;
+        }else{
+            return new ResponseEntity(new ArrayList[0], HttpStatus.OK);
         }
-        for(int i = solve.getAnswer().length-1 ; i > 0 ; i--){
-            for(int j = 0 ; j < i ; j++){
-                if( solve.getAnswer()[j] > solve.getAnswer()[j+1] ){
-                    int tmp = solve.getAnswer()[j];
-                    solve.getAnswer()[j] = solve.getAnswer()[j+1];
-                    solve.getAnswer()[j+1] = tmp;
-                }
-            }
-        }
-
-        if (quizzes.get(id - 1).getAnswer().length != solve.getAnswer().length) {
-            return answers.get(0);
-        }
-
-        for (int i = 0; i < solve.getAnswer().length; i++){
-            if (quizzes.get(id - 1).getAnswer()[i] != solve.getAnswer()[i]) {
-                return answers.get(0);
-            }
-        }
-        return answers.get(1);
-        /*
-        if (solve.getAnswer() == quizzes.get(id - 1).getAnswer() && solve.getAnswer().length > 0) {
-            System.out.println(quizzes.get(id - 1).getAnswer());
-            return answers.get(1);
-        } else {
-            return answers.get(0);
-        }
-
-         */
     }
+
+
+    @PostMapping(path = "/api/quizzes/{id}/solve", consumes = "application/json")
+    public Object solveQuiz(@PathVariable Integer id, @RequestBody Map<String, ArrayList<Integer>> answer){
+        Quiz quiz = quizRepository.findById(id)
+                .orElseThrow();
+        if (quiz!=null){
+            HashMap <String,Object> ans = new HashMap<>();
+            if(quiz.solveQuiz(answer.get("answer"))){
+                ans.put("success",true);
+                ans.put("feedback", "Congratulations, you're right!");
+            }else{
+                ans.put("success",false);
+                ans.put("feedback", "Wrong answer! Please, try again.");
+            }
+            return ans;
+
+        }else{
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
+        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
